a79baba45228747299d97e761b24659646fe1937
BYUNG-YONG KO <gooki69@gmail.com>
2026-01-04 12:25:32 +0900

    final

--- 26-01-03.py
+++ 26-01-03.py
@@ -5,10 +5,10 @@ arr = [[0] * 5 for _ in range(5)]
 sum_list = []
 l_diag = 0
 r_diag = 0
-for i in range(0,5):
+for i in range(0, 5):
     sum = 0
-    for j in range(0,5):
-         val = random.randrange(1,100)
+    for j in range(0, 5):
+         val = random.randrange(1, 100)
          arr[i][j] = val
          sum += val
          if j == 4:
@@ -19,8 +19,7 @@ for i in range(0,5):
 sum_list.append(l_diag)
 sum_list.append(r_diag)
 
-
-for i in range(0,5):
+for i in range(0, 5):
     print(f"{arr[i]} \t {i + 1}행합계 : {sum_list[i]}")
 print(f"\n왼쪽대각선합계 : {l_diag}\n오른쪽대각선합계 : {r_diag}")
 
